48e5db284b10b2cd657bb2f814177c29cd9c9b71
anupshakya07 <anupshakya07@gmail.com>
2022-02-10 14:52:36 -0600

    Committed all preprocessing steps

---
+++ preprocess.py
@@ -0,0 +1,59 @@
+import argparse
+import pickle
+from src.data_preprocessor import DataPreprocessor
+from src.obj2vec_embedder import RelationWordEmbedding
+from src.one_hot_encoder import OneHotEnc
+
+EMBEDDING_DIMENSION = 300
+
+
+def main(opt):
+    data_processor = DataPreprocessor(input_file_path=opt.dataset_path)
+    data_processor.analyze_dataset()
+    kc_maps = dict()
+    if opt.construct_vocab:
+        index_kc_map = dict([(index+5, kc) for index, kc in enumerate(data_processor.unique_kcs)])
+        kc_index_map = dict([(kc, index+5) for index, kc in enumerate(data_processor.unique_kcs)])
+        kc_maps["kc_index_map"] = kc_index_map
+        kc_maps["index_kc_map"] = index_kc_map
+        pickle.dump(kc_maps, open("output/kc_maps.pkl", "wb"))
+    else:
+        kc_maps = pickle.load(open("output/kc_maps.pkl", "rb"))
+
+    vector_embedder = RelationWordEmbedding(opt.dataset_path, embedding_dimension=EMBEDDING_DIMENSION,
+                                            sliding_window_size=8)
+    vector_embedder.train(tokenize_data=True, kc_maps=kc_maps)
+    vector_embedder.save_trained_model(opt.embedding_model_output)
+
+    one_hot_encoder = OneHotEnc()
+    one_hot_encoder.train(data_processor.unique_kcs)
+
+    collect_vector_embeddings(vector_embedder.model, data_processor)
+
+
+def collect_vector_embeddings(embedding_model, data_processor):
+    unique_student_embeddings = []
+    unique_problems_embeddings = []
+
+    for student in data_processor.unique_students:
+        if student in embedding_model.wv.vocab:
+            unique_student_embeddings.append(embedding_model.wv[student])
+
+    for problem in data_processor.unique_problems:
+        if problem in embedding_model.wv.vocab:
+            unique_problems_embeddings.append(embedding_model.wv[problem])
+
+    pickle.dump(unique_student_embeddings, open("input/unique_student_embeddings.pkl", "wb"))
+    pickle.dump(unique_problems_embeddings, open("input/unique_problems_embeddings.pkl", "wb"))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-dataset_path', type=str,
+                        default="dataset/bridge_to_algebra_2008_2009/sample_bridge_to_algebra_2008_2009_train.txt")
+    parser.add_argument('-embedding_model_output', type=str,
+                        default="embedding-models/gensim-bridge-to-algebra-embedding-model")
+    parser.add_argument('-construct_vocab', type=bool, default=False)
+
+    options = parser.parse_args()
+    main(options)
--- src/obj2vec_embedder.py
+++ src/obj2vec_embedder.py
@@ -2,7 +2,12 @@ import logging
 import pandas as pd
 import gensim.models
 import time
+import pickle
 
+from sklearn.model_selection import train_test_split
+
+BOS_TOKEN = 3
+EOS_TOKEN = 4
 
 class RelationWordEmbedding:
     def __init__(self, ip_path, embedding_dimension=300, sliding_window_size=8):
@@ -12,12 +17,14 @@ class RelationWordEmbedding:
         self.sentences = []
         self.model = None
 
-    def generate_sentences(self):
-        columns = ['Anon Student Id', 'Problem Hierarchy', 'Problem Name', 'KC(SubSkills)']
+    def generate_sentences(self, tokenize_data=True, kc_maps=None):
+        columns = ['Anon Student Id', 'Problem Hierarchy', 'Problem Name', 'KC(SubSkills)', 'Correct First Attempt']
         chunk_iterator = pd.read_csv(self.input_file_path, sep="\t", header=0, iterator=True, chunksize=1000000,
                                      usecols=columns)
         print("------Generating Sentences to train the word embeddings-------")
         start_time = time.time()
+        output_data = []
+        max_length = 0
 
         for chunk_data in chunk_iterator:
             filtered_chunk_data = chunk_data[chunk_data["KC(SubSkills)"].notnull()]
@@ -27,13 +34,19 @@ class RelationWordEmbedding:
                     prob_name = hierarchy_groups.groupby('Problem Name')
                     for problem_name, prob_name_groups in prob_name:
                         sub_skills = prob_name_groups['KC(SubSkills)']
+                        cfas = prob_name_groups['Correct First Attempt']
                         full_strategy = []
-                        for a in sub_skills:
-                            if str(a) != "nan":
-                                temp = a.split("~~")
+                        kcs_prob = [BOS_TOKEN]
+                        cfa_list = [BOS_TOKEN]
+                        for kc_row, cfa_row in zip(sub_skills, cfas):
+                            if str(kc_row) != "nan":
+                                correct_first_attempt = cfa_row
+                                temp = kc_row.split("~~")
                                 full_strategy.extend(temp)  # {KC1,KC2,....,KCn}
                                 self.sentences.append(temp)
-                                for kc in a.split("~~"):
+                                for kc in kc_row.split("~~"):
+                                    kcs_prob.append(kc_maps["kc_index_map"][kc])
+                                    cfa_list.append(correct_first_attempt)
                                     list_with_single_kc = [student_id, hierarchy, problem_name, kc]
                                     self.sentences.append(list_with_single_kc)  # {StdId, ProbHierarchy, ProblemName, KC}
                                     st_kc = [student_id, kc]
@@ -41,11 +54,29 @@ class RelationWordEmbedding:
                                     prob_kc = [problem_name, kc]
                                     self.sentences.append(prob_kc)
                         self.sentences.append(full_strategy)
+                        if 1 < len(kcs_prob) <= 150:
+                            cfa_list.append(EOS_TOKEN)
+                            kcs_prob.append(EOS_TOKEN)
+                            if len(kcs_prob) > max_length:
+                                max_length = len(kcs_prob)
+                            output_data.append({"src": kcs_prob, "target": cfa_list})
         end_time = time.time()
         print("Time Taken for generating the sentences = ", end_time - start_time)
+        if tokenize_data:
+            train, val = train_test_split(output_data, test_size=0.2, shuffle=True, random_state=40)
+            data_dump = {
+                "max_length": max_length,
+                "train": train,
+                "val": val,
+                "kc_index_map": kc_maps["kc_index_map"],
+                "index_kc_map": kc_maps["index_kc_map"],
+                "BOS": 3,
+                "EOS": 4
+            }
+            pickle.dump(data_dump, open("output/tokenized_data_BOS_EOS.pkl", "wb"))
 
-    def train(self):
-        self.generate_sentences()
+    def train(self, tokenize_data, kc_maps):
+        self.generate_sentences(tokenize_data=tokenize_data, kc_maps=kc_maps)
         print("******************************* Training Relational Word Embeddings *******************************")
         print("Parameters:->>>>>>>")
         print("Number of sentences trained = ", len(self.sentences))
